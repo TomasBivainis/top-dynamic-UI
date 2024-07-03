6401866f1d21d07d21f8b272c26a1f12e99b0ddb
Tomas Bivainis <bivainis.tomas@gmail.com>
2024-07-03 15:07:30 +0300

    slideshow now has circles which indicate the which picture is shown and can be clicked to show change the slide to the correspdoning one.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const divDropdown = document.querySelector("#dropdown");
 const buttonBack = document.querySelector("#back");
 const buttonForward = document.querySelector("#forward");
 const divPictures = document.querySelector("#pictures");
+const divButtons = document.querySelector("#buttons");
 
 let position = 0;
-const pictureAmount = (divPictures.childNodes.length - 1) / 2;
+const pictures = divPictures.querySelectorAll(".picture");
+const pictureAmount = pictures.length;
+let lastTime = Date.now();
 
 buttonDropdownButton.addEventListener("mouseenter", () => {
   divDropdownMenu.hidden = false;
@@ -21,17 +24,48 @@ buttonBack.addEventListener("click", goBack);
 buttonForward.addEventListener("click", goForward);
 
 function goForward() {
-  position++;
+  let index = position + 1;
 
-  position = position % pictureAmount;
+  index = index % pictureAmount;
 
-  divPictures.style.transform = `translateX(${-100 * position}px)`;
+  goToIndex(index);
 }
 
 function goBack() {
-  position--;
+  let index = position - 1;
 
-  if (position < 0) position += pictureAmount;
+  if (index < 0) index += pictureAmount;
 
+  goToIndex(index);
+}
+
+function goToIndex(index) {
+  position = index;
+  displayPicture();
+  displayDots();
+}
+
+function displayPicture() {
   divPictures.style.transform = `translateX(${-100 * position}px)`;
 }
+
+function displayDots() {
+  divButtons.innerHTML = "";
+
+  for (let i = 0; i < pictureAmount; i++) {
+    const buttonCircle = document.createElement("button");
+    buttonCircle.classList.add(`${i}`);
+    buttonCircle.classList.add("circle");
+    if (position === i) buttonCircle.classList.add("selected");
+
+    buttonCircle.addEventListener("click", (e) => {
+      goToIndex(parseInt(e.target.classList[0]));
+    });
+
+    divButtons.appendChild(buttonCircle);
+  }
+}
+
+displayDots();
+
+setInterval(goForward, 5000);
